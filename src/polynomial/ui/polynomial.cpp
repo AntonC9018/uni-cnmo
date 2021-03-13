c54f03f43e0f4665780f76dc9f5fe8e6dd4aa766
AntonC9018 <anton.curm@gmail.com>
2021-03-13 15:54:19 +0200

    move builtin functions in separate files

--- src/polynomial/ui/polynomial.cpp
+++ src/polynomial/ui/polynomial.cpp
@@ -1,15 +1,6 @@
-#include <func/func.h>
+#include "../builtin.h"
 
 namespace Poly
 {
-    static Expression_Func funcs[]
-    {
-        func_make(str_lit("x * exp(2 * sin(x))"), -3, 3),
-        func_make(str_lit("sin(x) * cos(x)"), -6, 6),
-        func_make(str_lit("sin(pi * x / 6) * cos(x - 1)"), -4, 4),
-        func_make(str_lit("exp(-x) - x^3 + 8 * cos(4 * x)"), -4, 4),
-        func_make(str_lit("x^3 - 5 * atan(x)"), -2, 2)
-    };
-
-    const size_t func_count = countof(funcs);
+    
 }
